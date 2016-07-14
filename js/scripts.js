85c79b8abcb368bcf47bd29e8cb2f3c877095342
Charlie Hayward <c.hayward.mail@gmail.com>
2016-07-13 20:52:23 -0700

    Add more logic to calc scores

--- js/scripts.js
+++ js/scripts.js
@@ -5,24 +5,32 @@ function Player(currentScore, totalScore, turnStatus) {
 }
 
 Player.prototype.rollDice = function() {
-  return Math.floor(Math.random() * 6) + 1;
-}
-
-var turnScore = function(currentScore) {
-  if(currentScore === 1) {
-    player1.turnStatus = false;
-    player2.turnStatus = true; 
+  var rollTurn = Math.floor(Math.random() * 6) + 1;
+  if (rollTurn === 1) {
+    this.currentScore = 0;
+    this.totalScore = 0;
+    this.turnStatus = false;
+  } else {
+    this.currentScore += rollTurn;
+    this.totalScore += this.currentScore;
   }
 }
 
-var player1 = new Player(0, 0, "True");
-var player2 = new Player(0, 0 , "False");
-
 $(document).ready(function() {
+  var player1 = new Player(0, 0, true);
+  var player2 = new Player(0, 0 , false);
 
   $("#playerOnePlay").click(function(event) {
-    var currentTurn = player1.rollDice();
-    $("#playerOneScore").text(currentTurn);
+    player1.rollDice();
+    if(player1.turnStatus === true) {
+      $("#playerOneCurrent").text(player1.currentScore);
+      $("#playerOneTotal").text(player1.totalScore);
+    } else {
+      $("#playerTwoPlay").click(function(event) {
+        player2.rollDice();
+        $("#playerTwoCurrent").text(player2.currentScore);
+        $("#playerTwoTotal").text(player2.totalScore);
+      });
+    }
   });
-
 });
